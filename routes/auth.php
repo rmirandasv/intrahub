f7722c88cca9bf7c54beba4c964ac482decb53a8
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-07-05 15:17:27 -0600

    refactor: remove registration functionality and associated routes

--- routes/auth.php
+++ routes/auth.php
@@ -12,11 +12,6 @@ use App\Http\Controllers\InvitationController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
@@ -34,7 +29,6 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 
-    // acept invitation
     Route::get(
         'accept-invitation/{token}',
         [InvitationController::class, 'accept']
